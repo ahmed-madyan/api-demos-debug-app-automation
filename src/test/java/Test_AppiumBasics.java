70afa013edd3cf2c380ca373952be6ad025ee862
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-05-18 17:16:13 +0300

    Create Test_AppiumBasics.java

---
+++ src/test/java/Test_AppiumBasics.java
@@ -0,0 +1,18 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Test_AppiumBasics extends TestBase {
+    @Test
+    public void appiumTest() throws MalformedURLException {
+
+    }
+}
