1446f5f1c80f4450cd96bfda92c11bbeb9e4be6e
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-05-18 17:27:16 +0300

    Update Test_AppiumBasics.java

--- src/test/java/Test_AppiumBasics.java
+++ src/test/java/Test_AppiumBasics.java
@@ -3,14 +3,64 @@ import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 import io.appium.java_client.service.local.flags.GeneralServerFlag;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class Test_AppiumBasics extends TestBase {
+public class Test_AppiumBasics {
+    private AppiumServiceBuilder serviceBuilder;
+    private AppiumDriverLocalService appiumService;
+    private static AndroidDriver androidDriver;
+
+    @BeforeTest
+    private void buildAppiumService() {
+        //Build the Appium service
+        serviceBuilder = new AppiumServiceBuilder()
+                .withIPAddress("127.0.0.1")
+                .usingPort(4723)
+                .withArgument(GeneralServerFlag.SESSION_OVERRIDE);
+    }
+
+    @BeforeTest(dependsOnMethods = "buildAppiumService")
+    private void startAppiumService() {
+        //Start the server with the builder
+        appiumService = AppiumDriverLocalService.buildService(serviceBuilder);
+        appiumService.start();
+    }
+
+    @BeforeTest(dependsOnMethods = "startAppiumService")
+    private void initDriver() {
+        //Initialize the driver and launch the app
+//        UiAutomator2Options options = new UiAutomator2Options();
+//        options.setDeviceName("Pixel 2 XL");
+//        options.setApp("C:\\Users\\_VOIS\\Documents\\GitHub\\rahul-appium-automation\\src\\test\\resources\\ApiDemos-debug.apk");
+        try {
+            androidDriver = new AndroidDriver(new URL("http://127.0.0.1:4723"),
+                    new UiAutomator2Options()
+                            .setDeviceName("Pixel 2 XL")
+                            .setApp("C:\\Users\\_VOIS\\Documents\\GitHub\\rahul-appium-automation\\src\\test\\resources\\ApiDemos-debug.apk"));
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @AfterTest
+    private void quitDriver() {
+        //Stop the server with the builder
+        androidDriver.quit();
+    }
+
+    @AfterTest(dependsOnMethods = "quitDriver")
+    private void stopAppiumService() {
+        //Stop the server with the builder
+        appiumService.stop();
+    }
+
+    public static AndroidDriver getDriver() {
+        return androidDriver;
+    }
+
     @Test
     public void appiumTest() throws MalformedURLException {
 
