00135cf1f67466fb1eb4a045123cee3ac2eb776d
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-05-24 18:28:37 +0300

    Update Test_SwipeScroll.java

--- src/test/java/Test_SwipeScroll.java
+++ src/test/java/Test_SwipeScroll.java
@@ -1,5 +1,8 @@
+import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -13,22 +16,25 @@ public class Test_SwipeScroll extends TestBase {
     private final By widgetListContent = AppiumBy.id("android:id/title");
 
     @Test
-    public void test_LongPress() throws InterruptedException {
-
-
-        Assert.assertTrue(DriverManager.getDriverInstance().findElement(views).isDisplayed());
-        MobileGestures.click(DriverManager.getDriverInstance(), views);
-        Assert.assertTrue(DriverManager.getDriverInstance().findElement(webView).isDisplayed());
-        MobileGestures.click(DriverManager.getDriverInstance(), webView);
-        Assert.assertTrue(DriverManager.getDriverInstance().findElement(customAdapter).isDisplayed());
-        MobileGestures.click(DriverManager.getDriverInstance(), customAdapter);
-        MobileGestures.longClick(DriverManager.getDriverInstance(), peopleNames, 2000);
-        Assert.assertTrue(DriverManager.getDriverInstance().findElement(widgetListView).isDisplayed());
-        Thread.sleep(5000);
-        System.out.println(DriverManager.getDriverInstance().findElements(widgetListTitle).get(0).getText() + "\n" + DriverManager.getDriverInstance().findElements(widgetListContent).get(1).getText());
-
-//        Assert.assertEquals(DriverManager.getDriverInstance().findElement(widgetListTitle).getText(), "Sample menu");
-//        Assert.assertEquals(DriverManager.getDriverInstance().findElement(widgetListContent).getText(), "Sample action");
+    public void test_SwipeScroll() {
+        Assert.assertTrue(ElementActions.findElement(views).isDisplayed());
+        MobileGestures.click(views);
+        ((JavascriptExecutor) DriverManager.getDriverInstance()).executeScript("mobile: scrollGesture", ImmutableMap.of(
+                "elementId", ((RemoteWebElement) DriverManager.getDriverInstance().findElement(views)).getId(),
+                "direction", "down",
+                "percent", 3.0
+        ));
+//        Assert.assertTrue(DriverManager.getDriverInstance().findElement(webView).isDisplayed());
+//        MobileGestures.click(DriverManager.getDriverInstance(), webView);
+//        Assert.assertTrue(DriverManager.getDriverInstance().findElement(customAdapter).isDisplayed());
+//        MobileGestures.click(DriverManager.getDriverInstance(), customAdapter);
+//        MobileGestures.longClick(DriverManager.getDriverInstance(), peopleNames, 2000);
+//        Assert.assertTrue(DriverManager.getDriverInstance().findElement(widgetListView).isDisplayed());
 //        Thread.sleep(5000);
+//        System.out.println(DriverManager.getDriverInstance().findElements(widgetListTitle).get(0).getText() + "\n" + DriverManager.getDriverInstance().findElements(widgetListContent).get(1).getText());
+//
+////        Assert.assertEquals(DriverManager.getDriverInstance().findElement(widgetListTitle).getText(), "Sample menu");
+////        Assert.assertEquals(DriverManager.getDriverInstance().findElement(widgetListContent).getText(), "Sample action");
+////        Thread.sleep(5000);
     }
 }
