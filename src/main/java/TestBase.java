c353119d5c34e5ad58b9a66c1b720e2c8b227204
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-05-25 18:14:59 +0300

    Update TestBase.java

--- src/main/java/TestBase.java
+++ src/main/java/TestBase.java
@@ -20,9 +20,54 @@ public class TestBase {
 
     private static final String browserStack_UserName = ("ahmedmadyan_oxTGNa");
     private static final String browserStack_AccessKey = ("efFbRGj4yaxcVuPxyJKs");
-//    private static String browserStack_ServerURL = ("https://" + browserStack_UserName + ":" + browserStack_AccessKey + "@hub-cloud.browserstack.com/wd/hub");
-//    private static DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-//    private static HashMap<String, Object> browserstackOptions = new HashMap<>();
+    private static DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+    private static HashMap<String, Object> browserstackOptions = new HashMap<>();
+
+    protected void browserStack_Setup() {
+        //Build the Browser Stack service
+        /**********************************************************************************************************/
+        /****************************************Specify the App***************************************************/
+        //Specify App
+        desiredCapabilities.setCapability("app", "CyberSafetyApp");
+        /**********************************************************************************************************/
+        /****************************************Select device*****************************************************/
+        //Select device
+        desiredCapabilities.setCapability("platformName", "android");
+        desiredCapabilities.setCapability("platformVersion", "9.0");
+        desiredCapabilities.setCapability("deviceName", "Google Pixel 3");
+        /**********************************************************************************************************/
+        /****************************************Select an automation engine***************************************/
+        //Select an automation engine
+        desiredCapabilities.setCapability("automationName", "UIAutomator2");
+        /**********************************************************************************************************/
+        /****************************************Organize tests****************************************************/
+        //Organize tests
+        browserstackOptions.put("projectName", "CyberSafetyApp");
+        browserstackOptions.put("buildName", "Regression build no.1");
+        /**********************************************************************************************************/
+        /****************************************Set debugging options*********************************************/
+        //Set debugging options
+        //1- Text logs are enabled by default, and cannot be disabled
+        //2- Network Logs are disabled by default. To enable network logs use its capability
+        browserstackOptions.put("networkLogs", "true");
+        //3- Device logs are enabled by default. To disable device logs use its capability
+        browserstackOptions.put("deviceLogs", "true");
+        //4- Appium logs are enabled by default. To disable Appium logs use its capability
+        browserstackOptions.put("appiumLogs", "true");
+        //5- Visual logs are disabled by default. To enable visual logs use its capability
+        browserstackOptions.put("debug", "true");
+        //6- Video logs are enabled by default. Note that video recording slightly increases the text execution time. To disable video logs use its capability
+        browserstackOptions.put("video", "true");
+        /**********************************************************************************************************/
+        /****************************************Set Appium version************************************************/
+        //Set Appium version
+        browserstackOptions.put("appiumVersion", "2.0.0");
+        /**********************************************************************************************************/
+        /*********************************Set browser stack capability options ************************************/
+        //Set bstack:options capabilities
+        desiredCapabilities.setCapability("bstack:options", browserstackOptions);
+        /**********************************************************************************************************/
+    }
 
     @BeforeClass
     protected void initDriver() {
