244a6ebb20f25b64768e32662f62c56fbdd3bde7
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-05-25 16:14:33 +0300

    Update TestBase.java

--- src/main/java/TestBase.java
+++ src/main/java/TestBase.java
@@ -3,42 +3,85 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
 
 public class TestBase {
     private AppiumServiceBuilder serviceBuilder;
     private AppiumDriverLocalService appiumService;
     private static AndroidDriver androidDriver;
 
+    private static final String browserStack_UserName = ("ahmedmadyan_oxTGNa");
+    private static final String browserStack_AccessKey = ("efFbRGj4yaxcVuPxyJKs");
+    private static String browserStack_ServerURL = ("https://" + browserStack_UserName + ":" + browserStack_AccessKey + "@hub-cloud.browserstack.com/wd/hub");
+    private static DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+    private static HashMap<String, Object> browserstackOptions = new HashMap<>();
+
     @BeforeClass
-    protected void buildAppiumService() {
-        //Build the Appium service
-        serviceBuilder = new AppiumServiceBuilder()
-                .withAppiumJS(new File("C:\\Users\\_VOIS\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js"))
-                .withIPAddress("127.0.0.1")
-                .usingPort(4723);
+    protected void browserStack_Setup() {
+        //Build the Browser Stack service
+
+        //Specify App
+//        desiredCapabilities.setCapability("app", "CyberSafetyApp");
+//
+//        //Select device
+//        desiredCapabilities.setCapability("device", "Google Pixel 3");
+//        desiredCapabilities.setCapability("os", "android");
+//        desiredCapabilities.setCapability("os_version", "10.0");
+//
+//        //Select an automation engine
+//        desiredCapabilities.setCapability("automationName", "UIAutomator2");
+//
+//        //Organize tests
+//        browserstackOptions.put("project", "API Demos Debug");
+//        browserstackOptions.put("build", "1");
+//        browserstackOptions.put("name", "API Demos Debug");
+////        browserstackOptions.put("buildTag", "Regression");
+////        desiredCapabilities.setCapability("bstack:options", browserstackOptions);
+//
+//        //Set debugging options
+//        //1- Text logs are enabled by default, and cannot be disabled
+//        //2- Network Logs are disabled by default. To enable network logs use its capability
+//        browserstackOptions.put("networkLogs", "true");
+////        desiredCapabilities.setCapability("bstack:options", browserstackOptions);
+//        //3- Device logs are enabled by default. To disable device logs use its capability
+//        browserstackOptions.put("deviceLogs", "true");
+////        desiredCapabilities.setCapability("bstack:options", browserstackOptions);
+//        //4- Appium logs are enabled by default. To disable Appium logs use its capability
+//        browserstackOptions.put("appiumLogs", "true");
+////        desiredCapabilities.setCapability("bstack:options", browserstackOptions);
+//        //5- Visual logs are disabled by default. To enable visual logs use its capability
+//        browserstackOptions.put("debug", "true");
+////        desiredCapabilities.setCapability("bstack:options", browserstackOptions);
+//        //6- Video logs are enabled by default. Note that video recording slightly increases the text execution time. To disable video logs use its capability
+//        browserstackOptions.put("video", "true");
+////        desiredCapabilities.setCapability("bstack:options", browserstackOptions);
+//
+//        //Set Appium version
+//        browserstackOptions.put("appiumVersion", "2.0.0");
+//        desiredCapabilities.setCapability("bstack:options", browserstackOptions);
+        // Use Java Client v6.0.0 or above
+        browserstackOptions.put("appiumVersion", "2.0.0");
+        browserstackOptions.put("debug", "true");
+        desiredCapabilities.setCapability("bstack:options", browserstackOptions);
+        desiredCapabilities.setCapability("platformName", "android");
+        desiredCapabilities.setCapability("platformVersion", "9.0");
+        desiredCapabilities.setCapability("deviceName", "Google Pixel 3");
+        desiredCapabilities.setCapability("app", "CyberSafetyApp");
+
     }
 
-    @BeforeClass(dependsOnMethods = "buildAppiumService")
-    protected void startAppiumService() {
-        //Start the server with the builder
-        appiumService = AppiumDriverLocalService.buildService(serviceBuilder);
-        appiumService.start();
-    }
-
-    @BeforeClass(dependsOnMethods = "startAppiumService")
+    @BeforeClass(dependsOnMethods = "browserStack_Setup")
     protected void initDriver() {
         //Initialize the driver and launch the app
         try {
-            androidDriver = new AndroidDriver(new URL("http://127.0.0.1:4723"),
-                    new UiAutomator2Options()
-                            .setDeviceName("Pixel 2 XL")
-                            .setApp("C:\\Users\\_VOIS\\Documents\\GitHub\\rahul-appium-automation\\src\\test\\resources\\ApiDemos-debug.apk"));
+            androidDriver = new AndroidDriver(new URL(browserStack_ServerURL), desiredCapabilities);
             Waits.visibilityOfElementLocated(AppiumBy.id("android:id/action_bar_container"));
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
@@ -49,12 +92,6 @@ public class TestBase {
     protected void tearDownDriver() {
         //Stop the server with the builder
         androidDriver.quit();
-    }
-
-    @AfterClass(dependsOnMethods = "tearDownDriver")
-    protected void stopAppiumService() {
-        //Stop the server with the builder
-        appiumService.stop();
     }
 
     protected static AndroidDriver getDriver() {
