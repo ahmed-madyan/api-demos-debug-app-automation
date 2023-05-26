2cf83f4562806ed8b6d24bb7e6373441ca91476d
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-05-27 01:10:17 +0300

    Update Test_Swipe.java

--- src/test/java/Test_Swipe.java
+++ src/test/java/Test_Swipe.java
@@ -7,12 +7,15 @@ public class Test_Swipe {
     private final By views = AppiumBy.accessibilityId("Views");
     private final By gallery = AppiumBy.accessibilityId("Gallery");
     private final By photos = AppiumBy.accessibilityId("1. Photos");
+    private final By imageView = AppiumBy.className("android.widget.ImageView");
+
     @Test
     public void test_Swipe() throws InterruptedException {
         Assert.assertTrue(ElementActions.findElement(views).isDisplayed());
         MobileGestures.click(views);
-        AppiumActions.scrollIntoView(webView, "WebView");
-        MobileGestures.click(webView);
+        MobileGestures.click(gallery);
+        MobileGestures.click(photos);
+        MobileGestures.swipe(DriverManager.getDriverInstance().findElements(imageView).get(0), MobileGestures.Direction.RIGHT);
         Thread.sleep(5000);
     }
 }
