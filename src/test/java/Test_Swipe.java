5481154e36c0eeb82b8299609a40a69ee937ce80
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-06-01 12:58:20 +0300

    Update Test_Swipe.java

--- src/test/java/Test_Swipe.java
+++ src/test/java/Test_Swipe.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class Test_Swipe {
+public class Test_Swipe extends TestBase {
     private final By views = AppiumBy.accessibilityId("Views");
     private final By gallery = AppiumBy.accessibilityId("Gallery");
     private final By photos = AppiumBy.accessibilityId("1. Photos");
@@ -15,7 +15,20 @@ public class Test_Swipe {
         MobileGestures.click(views);
         MobileGestures.click(gallery);
         MobileGestures.click(photos);
-        MobileGestures.swipe(DriverManager.getDriverInstance().findElements(imageView).get(0), MobileGestures.Direction.RIGHT);
+        int i = 0;
+//        boolean elementExist;
+//        do {
+//            System.out.println(DriverManager.getDriverInstance().findElements(imageView).);
+//            MobileGestures.swipe(DriverManager.getDriverInstance().findElements(imageView).get(i), MobileGestures.Direction.LEFT);
+//            i++;
+//            System.out.println(i);
+//            elementExist = DriverManager.getDriverInstance().findElements(imageView).get(i - 1).isDisplayed();
+//        } while (elementExist);
+        Assert.assertEquals(ElementActions.getAttribute(AppiumBy.xpath("//android.widget.ImageView[1]"), "focusable"), "true");
+        MobileGestures.swipe(AppiumBy.xpath("//android.widget.ImageView[1]"), MobileGestures.Direction.LEFT);
+//        for (int j = 0; j < ElementActions.findElements(imageView).size() - 1; j++) {
+//            MobileGestures.swipe(ElementActions.findElements(imageView).get(i), MobileGestures.Direction.LEFT);
+//        }
         Thread.sleep(5000);
     }
 }
