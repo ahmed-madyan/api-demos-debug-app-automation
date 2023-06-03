a7428a048dd2d9c2d0ce24eff9a549be125f0363
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-06-03 16:21:13 +0300

    Update Test_Swipe.java

--- src/test/java/Test_Swipe.java
+++ src/test/java/Test_Swipe.java
@@ -15,20 +15,8 @@ public class Test_Swipe extends TestBase {
         MobileGestures.click(views);
         MobileGestures.click(gallery);
         MobileGestures.click(photos);
-        int i = 0;
-//        boolean elementExist;
-//        do {
-//            System.out.println(DriverManager.getDriverInstance().findElements(imageView).);
-//            MobileGestures.swipe(DriverManager.getDriverInstance().findElements(imageView).get(i), MobileGestures.Direction.LEFT);
-//            i++;
-//            System.out.println(i);
-//            elementExist = DriverManager.getDriverInstance().findElements(imageView).get(i - 1).isDisplayed();
-//        } while (elementExist);
         Assert.assertEquals(ElementActions.getAttribute(AppiumBy.xpath("//android.widget.ImageView[1]"), "focusable"), "true");
         MobileGestures.swipe(AppiumBy.xpath("//android.widget.ImageView[1]"), MobileGestures.Direction.LEFT);
-//        for (int j = 0; j < ElementActions.findElements(imageView).size() - 1; j++) {
-//            MobileGestures.swipe(ElementActions.findElements(imageView).get(i), MobileGestures.Direction.LEFT);
-//        }
-        Thread.sleep(5000);
+        Assert.assertEquals(ElementActions.getAttribute(AppiumBy.xpath("//android.widget.ImageView[1]"), "focusable"), "false");
     }
 }
