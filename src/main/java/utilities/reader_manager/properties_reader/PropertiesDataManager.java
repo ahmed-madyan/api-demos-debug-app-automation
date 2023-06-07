cce9f18f273957a06f05aebab95dd37ea89cd0db
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-06-07 16:15:18 +0300

    Update PropertiesDataManager.java

--- src/main/java/utilities/reader_manager/properties_reader/PropertiesDataManager.java
+++ src/main/java/utilities/reader_manager/properties_reader/PropertiesDataManager.java
@@ -37,5 +37,43 @@ public class PropertiesDataManager {
         }
     }
 
+    public static String getProperty(String key, Capability capability) {
+        String filePathContentRoot = null;
+        switch (capability) {
+            case BROWSERSTACK_CAPABILITIES ->
+                    filePathContentRoot = ("/src/main/resources/config_properties/BrowserStackCapabilities.properties");
+            case EXECUTION_CAPABILITIES ->
+                    filePathContentRoot = ("/src/main/resources/config_properties/ExecutionPlatform.properties");
+            case MOBILE_CAPABILITIES ->
+                    filePathContentRoot = ("/src/main/resources/config_properties/MobileCapabilities.properties");
+        }
+        return readProperties(filePathContentRoot).getProperty(key).trim();
+    }
 
+    public static void setProperty(String key, String value, Capability capability) {
+        String filePathContentRoot = null;
+        switch (capability) {
+            case BROWSERSTACK_CAPABILITIES ->
+                    filePathContentRoot = ("/src/main/resources/config_properties/BrowserStackCapabilities.properties");
+            case EXECUTION_CAPABILITIES ->
+                    filePathContentRoot = ("/src/main/resources/config_properties/ExecutionPlatform.properties");
+            case MOBILE_CAPABILITIES ->
+                    filePathContentRoot = ("/src/main/resources/config_properties/MobileCapabilities.properties");
+        }
+        try {
+            OutputStream fileOutputStream = new FileOutputStream((PROJECT_PATH + filePathContentRoot));
+            properties.put(key, value.trim());
+            properties.store(fileOutputStream, null);
+        } catch (Exception e) {
+            System.out.println(e.getMessage() + "\n" + e.getMessage() + "\n");
+            e.printStackTrace();
+        }
+    }
+
+    public enum Capability {
+        BROWSERSTACK_CAPABILITIES,
+        EXECUTION_CAPABILITIES,
+        MOBILE_CAPABILITIES
+    }
 }
+
