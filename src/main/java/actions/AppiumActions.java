4dd9a7e98cd892d8a1e402630ba23bf30b8455f5
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-06-07 12:46:42 +0300

    UPDATED

--- src/main/java/actions/AppiumActions.java
+++ src/main/java/actions/AppiumActions.java
@@ -3,6 +3,7 @@ package actions;
 import hooks.DriverManager;
 import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.By;
+import org.openqa.selenium.DeviceRotation;
 import webdriver_waits.Waits;
 
 public class AppiumActions {
@@ -10,6 +11,40 @@ public class AppiumActions {
         try {
             DriverManager.getDriverInstance().findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"" + elementText + "\"));"));
             Waits.visibilityOfElementLocated(elementLocated);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setClipboardText(String text) {
+        try {
+            DriverManager.getDriverInstance().setClipboardText(text);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getClipboardText() {
+        String clipboardText = null;
+        try {
+            clipboardText = DriverManager.getDriverInstance().getClipboardText();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return clipboardText;
+    }
+
+    public static void rotateRight() {
+        try {
+            DriverManager.getDriverInstance().rotate(new DeviceRotation(0, 0, 90));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void rotateLeft() {
+        try {
+            DriverManager.getDriverInstance().rotate(new DeviceRotation(0, 0, 270));
         } catch (Exception e) {
             e.printStackTrace();
         }
---
+++ src/test/java/MiscellaneousOrientationAndKeyEventTest.java
@@ -0,0 +1,38 @@
+import actions.AppiumActions;
+import actions.ElementActions;
+import hooks.TestBase;
+import io.appium.java_client.AppiumBy;
+import mobile_gestures.MobileGestures;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MiscellaneousOrientationAndKeyEventTest extends TestBase {
+    private final By preference = AppiumBy.accessibilityId("Preference");
+    private final By preferenceFromXML = AppiumBy.accessibilityId("1. Preferences from XML");
+    private final By preferenceDependencies = AppiumBy.accessibilityId("3. Preference dependencies");
+    private final By wifiCheckBox = AppiumBy.xpath("//android.widget.CheckBox[@resource-id='android:id/checkbox']");
+    private final By wifiSettings = AppiumBy.xpath("//android.widget.TextView[@text='WiFi settings']");
+    private final By wifiSettingsName = AppiumBy.id("android:id/edit");
+    private final By wifiSettingsOkay = AppiumBy.id("android:id/button1");
+
+    @Test
+    public void miscellaneousRotation() {
+
+        MobileGestures.click(preference);
+        Assert.assertTrue(ElementActions.findElement(preferenceFromXML).isDisplayed());
+        MobileGestures.click(preferenceDependencies);
+        Assert.assertEquals(ElementActions.getAttribute(wifiCheckBox, "checked"), "false");
+        Assert.assertEquals(ElementActions.getAttribute(wifiSettings, "enabled"), "false");
+        MobileGestures.click(wifiCheckBox);
+        AppiumActions.rotateRight();
+        AppiumActions.rotateLeft();
+        Assert.assertEquals(ElementActions.getAttribute(wifiCheckBox, "checked"), "true");
+        Assert.assertEquals(ElementActions.getAttribute(wifiSettings, "enabled"), "true");
+        MobileGestures.click(wifiSettings);
+        AppiumActions.setClipboardText("Ahmed");
+        ElementActions.sendKeys(wifiSettingsName, AppiumActions.getClipboardText());
+        Assert.assertEquals(ElementActions.getText(wifiSettingsName), "Ahmed");
+        MobileGestures.click(wifiSettingsOkay);
+    }
+}
